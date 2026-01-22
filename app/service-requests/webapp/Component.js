e03143fa6d87f0997e99725eb2bf844e518cdddf
Syed Jafri <syedjafri06193@gmail.com>
2026-01-22 14:54:40 -0800

    Create Component.js

---
+++ app/service-requests/webapp/Component.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  "use strict";
+  return UIComponent.extend("servicerequests.ui.Component", {
+    metadata: {
+      manifest: "json"
+    }
+  });
+});
